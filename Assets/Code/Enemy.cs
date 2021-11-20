ee5f0ef9014151b66ab4140e3e47f357a92640bf
Felix Bernhardt <felix1.bernhardt@st.ovgu.de>
2021-11-20 01:06:38 +0100

    Enemyskript

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+[RequireComponent(typeof(Collider2D))]
+public class Enemy : MonoBehaviour {
+    private int lifePoints;
+    private bool inside => Camera.main.orthographicBounds().Intersects(GetComponent<Collider2D>().bounds);
         
+    // Start is called before the first frame update
+    void Start() {
+        StartCoroutine(lifeTimeRoutine());
+        StartCoroutine(lifePointsRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnDestroy() {
+        StopAllCoroutines();
+    }
+    void onHit(object origin) {
+        lifePoints--;
+    }
+
+    IEnumerator lifeTimeRoutine() {
+        yield return new WaitUntil(() => inside);
+        yield return new WaitWhile(() => inside);
+        Destroy(gameObject);
+    }
+
+    IEnumerator lifePointsRoutine() {
+        yield return new WaitWhile(() => lifePoints > 0);
+        Destroy(gameObject);
     }
 }
