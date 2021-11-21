5dfe6277c873037ca0c094fd9827aba7aa4ea9c5
arnes <arne.schaumburg@ovgu.de>
2021-11-21 18:05:55 +0100

    last changes

---
+++ Assets/Settings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.visible = false;
+    }
+}
